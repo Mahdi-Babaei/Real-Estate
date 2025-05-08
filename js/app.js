b0b2abc98be7d82ac124864a05380b23455117b9
Mahdi-Babaei <mahdibabaei.8626@gmail.com>
2025-05-08 13:19:33 +0330

    section-3 js codes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const $ = document
 const tabsDivElem = $.querySelectorAll(".tabsDiv")
 const browsePropertiesBtn = $.querySelectorAll("#browsePropertiesBtn")
 const firstSecTabs = $.querySelectorAll("#firstSecTabs")
+const propertiesContainer = $.querySelectorAll("#properties-container div")
+
+
 
 // HeroSection tab switch
 tabsDivElem.forEach(div => {
@@ -44,3 +47,57 @@ firstSecTabs.forEach(div => {
         div.classList.add("firstSecActiveTab")
     })
 })
+
+
+// Properties Box Generator
+propertiesContainer.forEach(div => {
+    div.className = 'relative rounded-lg border-[1.5px] border-[#F0EFFB] cursor-pointer bg-white inline-block'
+    div.insertAdjacentHTML("beforeend" , `
+                    <img src="./images/sections/${div.dataset.img}.png" alt="home" class="rounded-t-lg">
+                    <!-- Popular Badge Start -->
+                    ${isPopular(div.dataset.popular)}
+                    <!-- Popular Badge End -->
+                    <div class="px-5 py-5 flex flex-col gap-y-3">
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center">
+                                <h4 class="text-lightPurple font-bold text-2xl/[150%] pr-1">${div.dataset.price}</h4>
+                                <span class="text-sm/[140%] text-lightGray">/month</span>
+                            </div>
+                            <div class="flex items-center justify-center rounded-full h-12 w-12 border border-lightPink">
+                                <i class="fa-regular fa-heart fa-xl text-lightPurple"></i>
+                            </div>
+                        </div>
+                        <h2 class="text-2xl/[150%] font-bold text-mainColor">${div.dataset.name}</h2>
+                        <h5 class="text-base/[150%] text-lightGray border-b border-[#F0EFFB] pb-3">${div.dataset.address}</h5>
+                        <div class="flex gap-x-4">
+                            <div>
+                                <i class="fa-solid fa-bed text-lightPurple pr-1"></i>
+                                <span>${div.dataset.beds} Beds</span>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-bath text-lightPurple pr-1"></i>
+                                <span>${div.dataset.bathrooms} Bathrooms</span>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-expand text-lightPurple pr-1"></i>
+                                <span>${div.dataset.area} m²</span>
+                            </div>
+                        </div>
+                    </div>
+      `)
+
+})
+
+function isPopular (status) {
+    if (status === "true") {
+      return `    <div class="absolute -left-[15px] -mt-5">
+                        <div class="flex w-fit items-center justify-center py-2 px-3 gap-x-2 bg-lightPurple text-white font-jakarta font-bold rounded-t-lg rounded-br-lg">
+                            <i class="fa-solid fa-star"></i>
+                            <span>POPULAR</span>
+                        </div>
+                        <div id="triangle"></div>
+                    </div>`
+    } else {
+      return ` `
+    }
+}
